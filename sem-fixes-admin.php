49ad03d6056fe3fa96f145fea64140ac2fa91a46
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-12 14:46:42 +0000

    import plugins and sem-theme
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-fixes/trunk@1 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ sem-fixes-admin.php
@@ -0,0 +1,362 @@
+<?php
+
+class sem_fixes_admin
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		# sort admin menu
+		add_action('admin_menu', array('sem_fixes_admin', 'sort_admin_menu'), 1000000);
+		
+		# kill wp notifications (slows down the admin area)
+		remove_action( 'init', 'wp_version_check' );
+		remove_filter( 'update_footer', 'core_update_footer' );
+		remove_action( 'admin_notices', 'update_nag', 3 );
+		add_filter( 'pre_option_update_core', array('sem_fixes_admin', 'kill_wp_version_check'));
+				
+		# kill link updater
+		add_filter('option_use_linksupdate', create_function('$in', 'return 0;'));
+		
+		# allow html in term descriptions
+		add_action('init', array('sem_fixes_admin', 'set_kses'));
+		add_action('set_current_user', array('sem_fixes_admin', 'set_kses'));
+		
+		# fix plugins
+		add_action('plugins_loaded', array('sem_fixes_admin', 'fix_plugins'), 1000000);
+		
+		# Dashboard
+		add_action('load-index.php', array('sem_fixes_admin', 'dashboard_link'));
+		
+		#remove max width from admin screens
+		add_filter('tiny_mce_before_init', array('sem_fixes_admin', 'rmw_tinymce'));
+		add_action('admin_head', array('sem_fixes_admin', 'rmw_head'),99); //Hook late after all css has been done
+	} # init()
+	
+	
+	#
+	# dashboard_link()
+	#
+	
+	function dashboard_link()
+	{
+		if ( strpos($_SERVER['REQUEST_URI'], 'admin.php?page=index.php') !== false )
+		{
+			wp_redirect(trailingslashit(get_option('siteurl')) . 'wp-admin/');
+			die;
+		}
+	} # dashboard_link()
+	
+	
+	#
+	# kill_wp_version_check()
+	#
+	
+	function kill_wp_version_check($o)
+	{
+		global $wp_version;
+		
+		$o = (object) null;
+		$o->last_checked = time();
+		$o->version_checked = $wp_version;
+		
+		return $o;
+	} # kill_wp_version_check()
+	
+	
+	#
+	# set_kses()
+	#
+	
+	function set_kses()
+	{
+		remove_filter('pre_term_description', 'wp_filter_kses');
+		
+		if ( !current_user_can('unfiltered_html') )
+		{
+			add_filter('pre_term_description', 'wp_filter_post_kses');
+		}
+	} # term_kses()
+
+	#
+	# sort_admin_menu()
+	#
+
+	function sort_admin_menu()
+	{
+		global $submenu;
+
+		foreach ( $submenu as $key => $menu_items )
+		{
+			switch ( $key )
+			{
+			case 'post-new.php':
+			case 'edit.php':
+				$stop = 0;
+				$caps = array('edit_posts', 'edit_pages');
+				break;
+
+			case 'themes.php':
+			case 'plugins.php':
+				unset($menu_items[10]); # theme and plugin editors
+
+			default:
+				$stop = 1;
+				$caps = array();
+				break;
+			}
+
+			foreach ( $caps as $cap )
+			{
+				if ( current_user_can($cap) )
+				{
+					$stop++;
+				}
+			}
+
+			$unsortable = array();
+			$sortable = $menu_items;
+			reset($sortable);
+
+			while ( $stop != 0 )
+			{
+				$mkey = key($sortable);
+				$unsortable[$mkey] = current($sortable);
+				unset($sortable[$mkey]);
+
+				$stop--;
+			}
+
+			uasort($sortable, array('sem_fixes_admin', 'menu_nat_sort'));
+
+			$submenu[$key] = array_merge($unsortable, $sortable);
+			
+			if ( count($submenu[$key]) == 1 )
+			{
+				unset($submenu[$key]);
+			}
+		}
+	} # sort_admin_menu()
+
+
+	#
+	# menu_nat_sort()
+	#
+
+	function menu_nat_sort($a, $b)
+	{
+		return strnatcmp($a[0], $b[0]);
+	} # menu_nat_sort()
+	
+	
+	#
+	# fix_plugins()
+	#
+	
+	function fix_plugins()
+	{
+		# wp backup: major security flaws
+		if ( isset($GLOBALS['mywpdbbackup']) )
+		{
+			sem_fixes_admin::fix_db_backup();
+		}
+		
+		# now reading
+		if ( function_exists('nr_add_pages') )
+		{
+			add_action('load-settings_page_nr_options', array('sem_fixes_admin', 'nr_page_options'));
+			
+			remove_action('admin_menu', 'nr_add_pages');
+			add_action('admin_menu', array('sem_fixes_admin', 'nr_admin_menu'));
+			
+			remove_action('admin_head', 'nr_add_head');
+			add_action('admin_head', array('sem_fixes_admin', 'nr_admin_head'));
+		}
+		
+		# tinymce advanced
+		if ( function_exists('tadv_menu') ) 
+		{
+			remove_action('admin_menu', 'tadv_menu');
+			add_action('admin_menu', array('sem_fixes_admin', 'tinymce_advanced_admin_menu'));
+		}
+		
+	} # fix_plugins()
+	
+	
+	#
+	# fix_db_backup()
+	#
+	
+	function fix_db_backup()
+	{
+		global $mywpdbbackup;
+		
+		if ( isset($_POST['do_backup']) )
+		{
+			if ( $_POST['do_backup'] == 'backup' )
+			{
+				remove_action('admin_menu', array(&$mywpdbbackup, 'fragment_menu'));
+				add_action('admin_menu', array('sem_fixes_admin', 'db_backup_fragment_menu'));
+			}
+			elseif ( !current_user_can('administrator') )
+			{
+				remove_action('init', array(&$mywpdbbackup, 'perform_backup'));
+			}
+		}
+		elseif ( !isset($_GET['fragment']) && !isset($_GET['backup']) )
+		{
+			remove_action('admin_menu', array(&$mywpdbbackup, 'admin_menu'));
+			add_action('admin_menu', array('sem_fixes_admin', 'db_backup_admin_menu'));
+		}
+		
+	} # fix_db_backup()
+
+	
+	#
+	# db_backup_fragment_menu()
+	#
+	
+	function db_backup_fragment_menu()
+	{
+		global $mywpdbbackup;
+		add_management_page(__('Backup','wp-db-backup'), __('Backup','wp-db-backup'), 'administrator', $mywpdbbackup->basename, array(&$mywpdbbackup, 'build_backup_script'));
+	} # db_backup_fragment_menu()
+	
+	
+	#
+	# db_backup_admin_menu()
+	#
+	
+	function db_backup_admin_menu()
+	{
+		global $mywpdbbackup;
+		add_management_page(__('Backup','wp-db-backup'), __('Backup','wp-db-backup'), 'administrator', $mywpdbbackup->basename, array(&$mywpdbbackup, 'backup_menu'));
+	} # db_backup_admin_menu()
+	
+	
+	#
+	# nr_page_options()
+	#
+	
+	function nr_page_options()
+	{
+		ob_start(array('sem_fixes_admin', 'nr_page_options_ob'));
+	} # nr_page_options()
+	
+	
+	#
+	# nr_page_options_ob()
+	#
+	
+	function nr_page_options_ob($buffer)
+	{
+		return preg_replace_callback("/
+			<tr(?:\s[^>]*)?>
+			\s*
+			<th(?:\s[^>]*)?>(.*)<\/th>
+			.*
+			<\/tr>
+			/isUx", array('sem_fixes_admin', 'nr_page_options_callback'), $buffer);
+	} # nr_page_options_ob()
+	
+	
+	#
+	# nr_page_options_callback()
+	#
+	
+	function nr_page_options_callback($in)
+	{
+		if ( in_array($in[1],
+			array(
+				__('Date format string', NRTD) . ':',
+				__('Admin menu layout', NRTD) . ':',
+				__("HTTP Library", NRTD) . ':',
+				__("Use <code>mod_rewrite</code> enhanced library?", NRTD),
+				__("Debug mode", NRTD) . ':',
+				__("Multiuser mode", NRTD) . ':',
+				)
+			) )
+		{
+			return '';
+		}
+		
+		return $in[0];
+	} # nr_page_options_callback()
+	
+	
+	#
+	# nr_admin_menu()
+	#
+	
+	function nr_admin_menu()
+	{
+		add_submenu_page('post-new.php', 'Book Review', 'Book Review', 'edit_pages', 'add_book', 'now_reading_add');
+		add_management_page('Book Reviews', 'Book Reviews', 'edit_pages', 'manage_books', 'nr_manage');
+		add_options_page('Book Reviews', 'Book Reviews', 'manage_options', 'nr_options', 'nr_options');
+	} # nr_admin_menu()
+	
+	
+	#
+	# nr_admin_head()
+	#
+
+	function nr_admin_head()
+	{
+		echo '
+		<link rel="stylesheet" href="' . get_bloginfo('url') . '/wp-content/plugins/now-reading/admin/admin.css" type="text/css" />
+		<script type="text/javascript">
+			var lHide = "' . __("Hide", NRTD) . '";
+			var lEdit = "' . __("Edit", NRTD) . '";
+		</script>
+		<script type="text/javascript" src="' . get_bloginfo('url') . '/wp-content/plugins/now-reading/js/manage.js"></script>
+		';
+	} # nr_admin_head()
+
+	
+	#
+	# tinymce_advanced_admin_menu()
+	#
+	
+	function tinymce_advanced_admin_menu()
+	{
+		$page = add_options_page( 'TinyMCE Advanced', 'TinyMCE Advanced', 'manage_options', 'tinymce-advanced', 'tadv_page' );
+		add_action( "admin_print_scripts-$page", 'tadv_add_scripts' );
+		add_action( "admin_head-$page", 'tadv_admin_head' );
+	} #tinymce_advanced_admin_menu()	
+
+	/*
+	Remove Max Width functionality, version: 1.3
+	http://dd32.id.au/wordpress-plugins/remove-max-width/
+	by Dion Hulse (http://dd32.id.au/)
+	*/	
+	function rmw_tinymce($init)
+	{
+		$init['theme_advanced_resize_horizontal'] = true;
+		return $init;
+	}
+
+	function rmw_head()
+	{
+		global $is_IE; ?>
+		<style type="text/css" media="all">
+			.wrap, 
+			.updated,
+			.error,
+			#the-comment-list td.comment {
+				max-width: none !important;
+			}
+		<?php if( $is_IE )
+		{ ?>
+			* html #wpbody { 
+		 		_width: 99.9% !important; 
+		 	}
+		<?php } ?>
+		</style>
+	<?php } 
+	
+} # sem_fixes_admin
+
+sem_fixes_admin::init();
+?>
---
+++ sem-fixes.php
@@ -0,0 +1,446 @@
+<?php
+/*
+Plugin Name: Semiologic Fixes
+Plugin URI: http://www.getsemiologic.com
+Description: A variety of teaks and fixes for WordPress and third party plugins
+Author: Denis de Bernardy
+Version: 1.7
+Author URI: http://www.semiologic.com
+Update Service: http://version.semiologic.com/wordpress
+Update Tag: sem_fixes
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+define('sem_fixes_path', ABSPATH . PLUGINDIR);
+
+global $sem_fixes_files;
+global $sem_fixes_admin_files;
+
+$sem_fixes_files = array(
+	'impostercide.php',
+	'not-to-me.php',
+	'smart-update-pinger.php',
+	);
+$sem_fixes_admin_files = array(
+	'mypageorder/mypageorder.php',
+	);
+
+class sem_fixes
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		# auto-maintain db
+		add_action('maintain_db', array('sem_fixes', 'maintain_db'));
+
+		if ( !is_admin() )
+		{
+			if ( !wp_next_scheduled('maintain_db') )
+			{
+				wp_schedule_event(time(), 'hourly', 'maintain_db');
+			}
+
+			# remove #more-id in more links
+			add_filter('the_content', array('sem_fixes', 'fix_more'), 10000);
+
+			# fix wysiwyg
+			add_filter('the_content', array('sem_fixes', 'fix_wysiwyg'), 10000);
+		}
+		
+		# fix plugins
+		add_action('plugins_loaded', array('sem_fixes', 'fix_plugins'), 1000000);
+		
+		# fix widgets
+		add_action('init', array('sem_fixes', 'widgetize'));
+		
+		# security fix
+		add_filter('option_default_role', array('sem_fixes', 'default_role'));
+	
+		# disable post revisions
+		add_action('init', array('sem_fixes', 'disable_post_revisions'));
+		
+		# shortcodes
+		add_filter('get_the_excerpt', array('sem_fixes', 'strip_shortcodes'), 0);
+		add_filter('get_the_excerpt', array('sem_fixes', 'restore_shortcodes'), 20);
+		
+		# generator
+		add_filter('the_generator', array('sem_fixes', 'the_generator'));
+
+		# security fix		
+		add_filter('option_active_plugins', array('sem_fixes', 'kill_hack_files'));
+		
+		# tinyMCE
+		#add_filter('tiny_mce_before_init', array('sem_fixes', 'tiny_mce_config'));
+	} # init()
+	
+	
+	#
+	# tiny_mce_config()
+	#
+	
+	function tiny_mce_config($o)
+	{
+		# http://wordpress.org/support/topic/164217/page/2?replies=69#post-718683
+		
+		$o['compress'] = false;
+		
+		return $o;
+	} # tiny_mce_config()
+	
+	
+	#
+	# the_generator()
+	#
+	
+	function the_generator($in)
+	{
+		return '';
+	} # the_generator()
+	
+	
+	#
+	# strip_shortcodes()
+	#
+	
+	function strip_shortcodes($in = null)
+	{
+		remove_filter('the_content', 'do_shortcode', 11);
+		
+		return $in;
+	} # strip_shortcodes()
+	
+	
+	#
+	# restore_shortcodes()
+	#
+	
+	function restore_shortcodes($in = null)
+	{
+		add_filter('the_content', 'do_shortcode', 11);
+		
+		return $in;
+	} # restore_shortcodes()
+	
+	#
+	# disable_post_revisions()
+	#
+	
+	function disable_post_revisions()
+	{
+		remove_action ( 'pre_post_update', 'wp_save_post_revision' );
+		define('WP_POST_REVISIONS', false);
+	} # disable_post_revisions()	
+	
+
+	#
+	# default_role()
+	#
+	
+	function default_role($o)
+	{
+		if ( $o == 'administrator' && get_option('users_can_register') )
+		{
+			global $wp_roles;
+			
+			foreach ( $wp_roles->role_names as $role => $name )
+			{
+				if ( $role != 'administrator' )
+				{
+					$o = $role;
+					add_action('shutdown', create_function('', "update_option('default_role', '$role');"));
+					break;
+				}
+			}
+		}
+		
+		return $o;
+	} # default_role()
+	
+	
+	#
+	# widgetize()
+	#
+	
+	function widgetize()
+	{
+		global $wp_registered_widgets;
+		global $wp_registered_widget_controls;
+		
+		$wp_registered_widgets['calendar']['callback'] = array('sem_fixes', 'widget_calendar');
+		unset($wp_registered_widget_controls['calendar']);
+	} # widgetize()
+	
+	
+	#
+	# widget_calendar()
+	#
+	
+	function widget_calendar($args)
+	{
+		extract($args);
+		echo $before_widget;
+		if ( !is_admin() )
+		{
+			echo '<div id="calendar_wrap">';
+			get_calendar();
+			echo '</div>';
+		}
+		echo $after_widget;
+	} # widget_calendar()
+	
+
+
+	#
+	# maintain_db()
+	#
+
+	function maintain_db()
+	{
+		global $wpdb;
+
+		$tablelist = $wpdb->get_results("SHOW TABLES", ARRAY_N);
+
+		foreach ($tablelist as $table)
+		{
+			$tablename = $table[0];
+
+			$check = $wpdb->get_row("CHECK TABLE $tablename", ARRAY_N);
+
+			if ( $check[2] == 'error' )
+			{
+				if ( $check[3] == 'The handler for the table doesn\'t support check/repair' )
+				{
+					continue;
+				}
+				else
+				{
+					$repair = $wpdb->get_row("REPAIR TABLE $tablename", ARRAY_N);
+
+					if ( $repair[3] != 'OK' )
+					{
+						continue;
+					}
+				}
+			}
+
+			$wpdb->query("OPTIMIZE TABLE $tablename");
+		}
+	} # maintain_db()
+	
+	
+	#
+	# fix_more()
+	#
+
+	function fix_more($content)
+	{
+		if ( is_singular() || !in_the_loop() )
+		{
+			return $content;
+		}
+		else
+		{
+			return str_replace(
+				"#more-" . get_the_ID(),
+				'',
+				$content
+				);
+		}
+	} # fix_more()
+	
+	
+	#
+	# fix_wysiwyg()
+	#
+	
+	function fix_wysiwyg($content)
+	{
+		if ( ( $option = get_option('fix_wysiwyg') ) === false )
+		{
+			add_option('fix_wysiwyg', '0');
+		}
+		elseif ( !$option )
+		{
+			return $content;
+		}
+		
+		$find_replace = array(
+			# broken paragraph tag
+			"~
+				<p>\s*&lt;\s*</p>
+				\s*
+				<p>p(?:>|&gt;>)
+			~isx" => "<p>",
+			
+			# closed p, div or noscript singleton
+			"~
+				<\s*(?:p|div|noscript)	# p, div or noscript tag
+				(?:\s[^>]*)?			# optional attributes
+				/\s*>					# />
+			~ix" => "",
+			
+			# empty paragraph
+			"~
+				<p></p>					# empty paragraph
+			~ix" => "",
+			
+			# broken div align
+			"~
+				<p>&lt;</p>
+				\s*
+				<p>div
+					\s+
+					align=(?:&\#034;|&\#8221;)right(?:&\#034;|&\#8221;)
+					>
+				(.*?)
+				</p>
+			~isx" => "<p style=\"text-align: right;\">$1</p>",
+			
+			# more|nextpage in div tag
+			"~
+				<div\s+align=\"right\">(<!--(?:more|nextpage)-->)</div>
+			~isx" => "<p style=\"text-align: right;\">$1</p>",
+			);
+		
+		$find = array();
+		$replace = array();
+		
+		foreach ( $find_replace as $key => $val )
+		{
+			$find[] = $key;
+			$replace[] = $val;
+		}
+		
+		$content = preg_replace(
+			$find,
+			$replace,
+			$content
+			);
+		
+		return $content;
+	} # fix_wysiwyg()
+	
+	
+	#
+	# fix_plugins()
+	#
+	
+	function fix_plugins()
+	{
+		# easy auction ads
+		if ( function_exists('wp_easy_auctionads_start') )
+		{
+			add_action('before_the_wrapper', 'wp_easy_auctionads_start');
+			add_action('after_the_wrapper', 'wp_easy_auctionads_end');
+		}
+		
+		# now reading
+		if ( function_exists('nr_add_pages') )
+		{
+			add_filter('option_nowReadingOptions', array('sem_fixes', 'nr_options'));
+			remove_action('wp_head', 'nr_header_stats');
+		}
+		
+		# hashcash
+		if ( function_exists('wphc_add_commentform') )
+		{
+			remove_action('admin_menu', 'wphc_add_options_to_admin');
+			remove_action('comment_form', 'wphc_add_commentform');
+			remove_action('widgets_init', 'wphc_widget_init');
+			
+			add_action('comment_form', array('sem_fixes', 'wphc_addform'));
+		}
+	} # fix_plugins()
+	
+	
+	#
+	# function nr_options()
+	#
+	
+	function nr_options($o)
+	{
+		$useModRewrite = $o['useModRewrite'];
+		
+		$o['menuLayout'] = NR_MENU_MULTIPLE;
+		$o['useModRewrite'] = intval(get_option('permalink_structure') != '');
+		$o['debugMode'] = 0;
+		$o['httpLib'] = function_exists('curl_init') ? 'curl' : 'snoopy';
+		$o['formatDate'] = get_option('date_format');
+		$o['permalinkBase'] = 'library/';
+		$o['booksPerPage'] = $o['booksPerPage'] ? $o['booksPerPage'] : 10;
+		
+		if ( $useModRewrite != $o['useModRewrite'] )
+		{
+			remove_filter('option_nowReadingOptions', array('sem_fixes', 'nr_options'));
+			update_option('nowReadingOptions', $o);
+
+			$GLOBALS['wp_rewrite'] =& new WP_Rewrite();
+			$GLOBALS['wp_rewrite']->flush_rules();
+		}
+		
+		return $o;
+	} # nr_options()
+	
+	
+	#
+	# wphc_addform()
+	#
+	
+	function wphc_addform()
+	{
+		$options = wphc_option();
+
+		switch($options['moderation']){
+			case 'delete':
+				$verb = 'deleted';
+				break;
+			case 'akismet':
+				$verb = 'queued in Akismet';
+				break;
+			case 'moderate':
+			default:
+				$verb = 'placed in moderation';
+				break;
+		}
+
+		echo '<input type="hidden" id="wphc_value" name="wphc_value" value=""/>';
+		echo '<noscript><small>Wordpress Hashcash needs javascript to work, but your browser has javascript disabled. Your comment will be '.$verb.'!</small></noscript>';
+	} # wphc_addform()
+
+	#
+	# kill_hack_files()
+	#
+	function kill_hack_files($files)
+	{
+		foreach ( (array) $files as $k => $v )
+		{
+			if ( strpos($v, '..') !== false )
+			{
+				// maybe log the issue and auto-correct the option
+				unset($files[$k]);
+			}
+		}
+		return $files;
+	}
+
+} # sem_fixes
+
+sem_fixes::init();
+
+
+if ( is_admin() )
+{
+	include dirname(__FILE__) . '/sem-fixes-admin.php';
+}
+?>
