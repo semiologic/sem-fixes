65735fb38cb28d34d13b57723e51a147308db3f2
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-20 12:58:17 +0000

    automatically add thickbox to images when in presence of a direct link
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-fixes/trunk@238 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-fixes.php
+++ sem-fixes.php
@@ -85,8 +85,69 @@ class sem_fixes
 			add_action('wp_print_styles', array('sem_fixes', 'add_css'));
 			
 			add_action('wp_head', array('sem_fixes', 'add_thickbox_images'), 20);
+			
+			add_filter('the_content', array('sem_fixes', 'add_thickbox'), 20);
+			add_filter('the_excerpt', array('sem_fixes', 'add_thickbox'), 20);
 		}
 	} # init()
+	
+	
+	#
+	# add_thickbox()
+	#
+	
+	function add_thickbox($content)
+	{
+		$content = preg_replace_callback("/
+			<\s*a\s					# an achnor...
+				(.*?\s)?
+				href\s*=\s*(.+?)	# (catch href)
+				(\s.*?)?
+				>\s*
+			(<\s*img\s.+?>)\s*		# ... on an image
+			<\s*\/\s*a\s*>
+			/isx", array('sem_fixes', 'add_thickbox_callback'), $content);
+		
+		return $content;
+	} # add_thickbox()
+	
+	
+	#
+	# add_thickbox_callback()
+	#
+	
+	function add_thickbox_callback($match)
+	{
+		# trim surrounding quotes
+		$href = trim(trim($match[2]), '\'"');
+		
+		# return if not an image
+		if ( !preg_match("/\.(jpe?g|gif|png)$/i", $href) ) return $match[0];
+		
+		$match[3] = ' class="blah"';
+		# add thickbox class
+		$attr = ' ' . $match[1] . $match[3] . ' ';
+		$img = $match[4];
+		
+		if ( !preg_match("/
+				(\sclass\s*=\s*(.+?))(?:$|\s[a-z]+=)
+			/ix", $attr, $class)
+			)
+		{
+			$attr .= ' class="thickbox"';
+		}
+		else
+		{
+			# trim surrounding quotes
+			$old_class = trim(trim($class[2]), '\'"');
+			$new_class = $old_class . ' thickbox';
+			
+			# replace class
+			$attr = str_replace($class[0], 'class="' . $new_class . '"', $attr);
+		}
+		
+		return '<a href="' . $href . '" ' . $attr . '>' . $img . '</a>';
+	} # add_thickbox_callback()
 	
 	
 	#
