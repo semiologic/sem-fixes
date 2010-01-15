a15460d52e250de441faa9ad815d224218956a64
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-01-15 19:11:33 +0000

    make the WP timezone fix consistent
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-fixes/trunk@3955 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-fixes.php
+++ sem-fixes.php
@@ -41,7 +41,7 @@ if ( @ini_get('pcre.recursion_limit') <= 250000 )
 
 # fix calendar, see http://core.trac.wordpress.org/ticket/9588
 if ( function_exists('date_default_timezone_set') )
-	date_default_timezone_set(ini_get('date.timezone') ? ini_get('date.timezone') : 'UTC');
+	date_default_timezone_set('UTC');
 wp_timezone_override_offset();
 
 class sem_fixes {
